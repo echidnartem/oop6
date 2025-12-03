a26ba5b3e79b77735c9ee620bff675604346c704
Artyom Mitskevich <artemkurskzoloto@mail.ru>
2025-12-03 17:08:16 +0300

    Lab 6

---
+++ source/observer.cpp
@@ -0,0 +1,26 @@
+#include "../include/observer.h"
+
+
+void FileObserver::update(const NPC &attacker, const NPC &defender) {
+    log_file.open("../log.txt", std::ios::app);
+
+    if (log_file.is_open()) {
+        log_file << "[file observer]: ";
+        attacker.save(log_file);
+        log_file << "kills ";
+        defender.save(log_file);
+        log_file << '\n';
+    }
+    else {
+        throw std::logic_error("File could not be opened");
+    }
+    log_file.close();
+}
+
+void ConsoleObserver::update(const NPC &attacker, const NPC &defender) {
+    std::cout << "[console observer]: ";
+    attacker.print(std::cout) << ' ';
+    std::cout << "kills ";
+    defender.print(std::cout);
+    std::cout << '\n';
+}
